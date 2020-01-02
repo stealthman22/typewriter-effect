97a93f48fd156f8ecbf3ece67a8ed25b413a3920
stealthman22 <tdotonline@gmail.com>
2020-01-02 23:47:50 +0000

    Buidking constructor fucntion

--- index.js
+++ index.js
@@ -8,5 +8,10 @@ const TypeWriter = function (txtElement, words, wait = 3000) {
     this.txtElement = txtElement;
     this.words = words;
     this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
 
 }
+
